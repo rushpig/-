04149fb24d17e17f5f408d29b11f9100726976b6
xsy <XSY1162022846@outlook.com>
2023-10-31 23:53:52 +0800

    前后端管理系统创建

--- guilmall-coupon/src/main/java/com/example/guilmallcou/pon/GuilmallCouponApplication.java
+++ guilmall-coupon/src/main/java/com/example/guilmallcou/pon/GuilmallCouponApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GuilmallCouponApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GuilmallCouponApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(GuilmallCouponApplication.class, args);
+	}
 
 }
